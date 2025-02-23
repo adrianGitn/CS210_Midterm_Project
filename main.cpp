ae3e633fa583e10177528b6e9595d647d9f797f0
adrianGitn <adrgonz0823@gmail.com>
2025-02-22 21:39:45 -0600

    Update main.cpp
    Currently displays linked list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include "CSVReader.h"
+#include "SchoolList.cpp"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    SchoolList list;
+    CSVReader reader;
+
+    vector<vector<string>> data;
+    data = reader.readCSV("schoolList.csv");
+
+    for (size_t i = 1; i < data.size(); i++) {
+        School school(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
+        list.insertFirst(school);
+    }
+    list.display();
+    
+
+
+
+
+
 }
