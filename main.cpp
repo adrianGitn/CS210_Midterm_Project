81d57995cb93a6b849f823d47d56a8e75b8c4e30
adrianGitn <adrgonz0823@gmail.com>
2025-02-25 12:44:56 -0600

    Update main.cpp
    updated main file to take in user input in a loop

--- main.cpp
+++ main.cpp
@@ -15,13 +15,40 @@ int main() {
         School school(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
         list.insertFirst(school);
     }
-    list.display();
 
-    list.deleteByName("KELLAR PRIMARY SCHOOL");
+    bool run = true;
+    int input;
+    string nameInput;
+    while (run) {
+        cout << "Enter 1 for display, 2 to find by name, 3 for delete by name, or 4 for exit: " << endl;
+        cin >> input;
 
-    list.display();
+        switch (input){
+            case(1):
+                list.display();
+                break;
+            case(2):
+                cout << "Enter name to find: " << endl;
+                cin.ignore();
+                getline(cin, nameInput);
+                list.findByName(nameInput);
+                break;
+            case(3):
+                cout << "Enter name to delete: " << endl;
+                cin.ignore();
+                getline(cin, nameInput);
+                list.deleteByName(nameInput);
+                break;
+            default:
+                cout << "Ending process, have a nice day." << endl;
+                run = false;
+                break;
 
-    list.findByName("PLEASANT VALLEY MIDDLE SCHOOL");
+        }
+
+
+    }
+
 
 
 
