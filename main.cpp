a615282ad2ae90336e47952bd8ad11f396954b24
adrianGitn <adrgonz0823@gmail.com>
2025-03-06 10:38:13 -0600

    Update main.cpp
    modified functionality for bst tree

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include "CSVReader.h"
-#include "SchoolList.cpp"
+//#include "SchoolList.cpp"
+#include "SchoolBST.h"
+
 using namespace std;
 
 int main() {
-    SchoolList list;
+    //SchoolList list;
     CSVReader reader;
+    SchoolBST bst;
+
 
     vector<vector<string>> data;
     data = reader.readCSV("schoolList.csv");
 
-    for (size_t i = 1; i < data.size(); i++) {
+
+    /*for (size_t i = 1; i < data.size(); i++) {
         School school(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
         list.insertFirst(school);
+    }*/
+    // milestone 01 insert algorithm
+
+    for (size_t i = 1; i < data.size(); i++)
+    {
+        BSTSchool school(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
+        bst.insert(school);
     }
 
     bool run = true;
@@ -25,19 +37,19 @@ int main() {
 
         switch (input){
             case(1):
-                list.display();
+                bst.inOrder();
                 break;
             case(2):
                 cout << "Enter name to find: " << endl;
                 cin.ignore();
                 getline(cin, nameInput);
-                list.findByName(nameInput);
+                bst.find(nameInput);
                 break;
             case(3):
                 cout << "Enter name to delete: " << endl;
                 cin.ignore();
                 getline(cin, nameInput);
-                list.deleteByName(nameInput);
+                //bst.deleteByName(nameInput);
                 break;
             default:
                 cout << "Ending process, have a nice day." << endl;
